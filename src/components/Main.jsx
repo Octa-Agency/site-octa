62bf0ba9ff4bda561f4b03b66edb5b26884cd566
Arthur Gonçalves <arthurgoncalvesa2022@gmail.com>
2025-06-09 03:39:26 -0300

    first commit/site-teste

---
+++ src/components/Main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import tree from "../assets/tree.png";
+import logo from "../assets/logo2.png";
+function Main() {
+  return (
+    <>
+      <div className="main">
+        <div className="main-banner"></div>
+        <div className="tittle">
+          <img src={logo} alt="" />
+          <div className="tittle__subtittle">
+            <p>
+              Lorem um is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <button className="button">Button</button>
+        </div>
+        <div className="main-banner__tree">
+          <img src={tree} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
